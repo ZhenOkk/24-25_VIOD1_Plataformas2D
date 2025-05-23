d9b224d66fd11fd5c4fc0750ba5182b46519d1d9
Zhen <iago.merida@enti.cat>
2025-05-23 15:59:36 +0200

    Git iniciado + Movimiento2D

---
+++ Assets/Scripts/Movimiento2D.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento2D : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [Header("Movimiento")]
+    private float movimientoHorizontal = 0f;
+    [SerializeField] private float speed;
+    [SerializeField] private float speedSmooth;
+    private Vector3 velocidad = Vector3.zero;
+    private bool mirandoDerecha = true;
+
+    [Header("Salto")]
+    [SerializeField] private float jumpStrenght;
+    [SerializeField] private LayerMask isGround;
+    [SerializeField] private Transform groundController;
+    [SerializeField] private Vector3 boxDimensions;
+    [SerializeField] private bool touchGround;
+    private bool jump = false;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movimientoHorizontal = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        touchGround = Physics2D.OverlapBox(groundController.position, boxDimensions, 0f, isGround);
+        Movimiento(movimientoHorizontal * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+    private void Movimiento(float mover, bool jump)
+    {
+        Vector3 objectSpeed = new Vector2(mover, rb2D.velocity.y);
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, objectSpeed, ref velocidad, speedSmooth);
+
+        if (mover > 0 && !mirandoDerecha)
+        {
+            //Girar
+            Girar();
+        }
+        else if (mover < 0 && mirandoDerecha)
+        {
+            //Girar
+            Girar();
+        }
+
+        if (touchGround && jump)
+        {
+            touchGround = false;
+            rb2D.AddForce(new Vector2(0f, jumpStrenght));
+        }
+    }
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(groundController.position, boxDimensions);
+    }
+}
