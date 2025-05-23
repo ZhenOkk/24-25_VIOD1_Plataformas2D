6ffc8ecfbc1f18b8216c85cbd43edfd5c43dfdb1
Zhen <iago.merida@enti.cat>
2025-05-23 18:55:32 +0200

    Respawn
    Finished the respawn functionality.

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    Vector2 startPos;
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DeathVoid"))
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Reespawn();
+    }
+    void Reespawn()
+    {
+        transform.position = startPos;
+    }
+}
